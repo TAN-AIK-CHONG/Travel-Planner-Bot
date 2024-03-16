07b4de848043538e412a96f99b66ad5c59882eba
niclee1803 <niclee1803@outlook.com>
2024-03-16 19:02:20 +0800

    fix multiple users problem

--- bot.py
+++ bot.py
@@ -6,9 +6,8 @@ import requests
 load_dotenv('.env')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-
 bot = telebot.TeleBot(BOT_TOKEN)
-flight_info = {}
+users = {}
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -16,32 +15,39 @@ def send_welcome(message):
     
 @bot.message_handler(commands=['flight'])
 def ask_origin(message):
+    chat_id = message.chat.id
+    users[chat_id] = {"flight_info": {}}
     bot.reply_to(message, "What is your home country?")
     bot.register_next_step_handler(message, ask_depart)
     
 def ask_depart(message):
-    flight_info["partner_market"] = message.text
+    chat_id = message.chat.id
+    users[chat_id]["flight_info"]["partner_market"] = message.text
     bot.reply_to(message, "Which city is the flight departing from?")
     bot.register_next_step_handler(message, ask_arrive)
     
 def ask_arrive(message):
-    flight_info["fly_from"] = message.text
+    chat_id = message.chat.id
+    users[chat_id]["flight_info"]["fly_from"] = message.text
     bot.reply_to(message, "Which city does the flight arrive at?")
     bot.register_next_step_handler(message, ask_date)
     
 def ask_date(message):
-    flight_info["fly_to"] = message.text
+    chat_id = message.chat.id
+    users[chat_id]["flight_info"]["fly_to"] = message.text
     bot.reply_to(message, "Please input the date of the flight in DD.MM.YYYY format. eg. 24.04.2024 is 24 April 2024")
     bot.register_next_step_handler(message, ask_return)
     
 def ask_return(message):
-    flight_info["date_from"] = message.text
+    chat_id = message.chat.id
+    users[chat_id]["flight_info"]["date_from"] = message.text
     bot.reply_to(message, "When is the return flight?")
     bot.register_next_step_handler(message, confirmation)
 
 def confirmation(message):
-    flight_info["return_from"] = message.text
+    chat_id = message.chat.id
+    users[chat_id]["flight_info"]["return_from"] = message.text
+    flight_info = users[chat_id]["flight_info"]
     bot.reply_to(message, f"Confirm your details\nHome Country: {flight_info['partner_market']}\nDeparture City: {flight_info['fly_from']}\nArrival City: {flight_info['fly_to']}\nFlight date: {flight_info['date_from']}\nReturn Date: {flight_info['return_from']}")
     
-        
 bot.infinity_polling()  
