1d89f541dfcd9ff9e8ddc10e1559409fb34fca30
Tan Aik Chong <atan160@e.ntu.edu.sg>
2024-03-16 12:37:59 +0800

    updates

--- bot.py
+++ bot.py
@@ -1,18 +1,30 @@
 import os
 from dotenv import load_dotenv
 import telebot
+from flask import Flask, request,  jsonify, render_template
+import requests
 
 load_dotenv('.env')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+GEOFINDER_KEY = os.getenv('GEOFINDER_KEY')
 
 bot = telebot.TeleBot(BOT_TOKEN)
+user_states ={}
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Hi, I'm ExpeditionExpertBot! Here's what I can do:")
+    bot.reply_to(message, "Hi, I'm ExpeditionExpertBot! I'll help you plan an itinerary for your vacation. /itinerary to begin.")
     
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['itinerary'])
+def ask_city(message):
+    user_states[message.chat.id] = 'waiting_for_city'
+    bot.reply_to(message, "Which city are you planning on visiting?")
     
+@bot.message_handler(content_types=['location'], func=lambda message: user_states.get(message.chat.id) == 'waiting_for_city')
+def process_location(message):
+    latitude = message.location.latitude
+    longitude = message.location.longitude
+    del user_states[message.chat.id]  # Remove the user state
+    bot.reply_to(message, f"The location latitude is {latitude} and the location longitude is {longitude}")
+        
 bot.infinity_polling()
